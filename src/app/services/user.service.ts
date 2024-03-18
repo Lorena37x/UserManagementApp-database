6730b54e3f60cb30ec11dabf0e1d01d5b850dc07
Lorena Papeša <lorena.papesa37@gmail.com>
2024-03-18 14:33:14 +0100

    headers

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PrehranaView } from '../core/models/prehrana-view';
@@ -8,6 +8,7 @@ import { UsernameView } from '../core/models/username-view';
 import { PasswordView } from '../core/models/password-view';
 import { RoleView } from '../core/models/role-view';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -16,64 +17,86 @@ export class UserService {
 
   constructor(private _http: HttpClient) {}
 
+  private getHeaders(): HttpHeaders {
+    const headers = new HttpHeaders({
+      'Name': 'Lorena',
+    });
+    return headers;
+  }
+
   getUserData(id: number): Observable<UserView> {
-    return this._http.get<UserView>(`http://localhost:5013/Users/GetUser?userId=${id}`);
+    const headers = this.getHeaders();
+    return this._http.get<UserView>(`http://localhost:5013/Users/GetUser?userId=${id}`, { headers });
   }
 
   getNutritionData(id: number): Observable<PrehranaView[]> {
-    return this._http.get<PrehranaView[]>(`http://localhost:5013/Users/GetNutrition?userId=${id}`);
+    const headers = this.getHeaders();
+    return this._http.get<PrehranaView[]>(`http://localhost:5013/Users/GetNutrition?userId=${id}`, { headers });
   }
   
 
   getUserList(): Observable<UserView[]> {
-    return this._http.get<UserView[]>('http://localhost:5013/Users/GetUsers');
+    const headers = this.getHeaders();
+    return this._http.get<UserView[]>('http://localhost:5013/Users/GetUsers', { headers });
   }
 
   getFoodList(id: number): Observable<any[]> {
-    return this._http.get<any[]>(`http://localhost:5013/Users/GetFood?userId=${id}`);
+    const headers = this.getHeaders();
+    return this._http.get<any[]>(`http://localhost:5013/Users/GetFood?userId=${id}`, { headers });
   }
 
   addUser(data: UserView): Observable<any> {
-    return this._http.post('http://localhost:5013/Users/PostUsers', data);
+    const headers = this.getHeaders();
+    return this._http.post('http://localhost:5013/Users/PostUsers', data, { headers });
   }
 
   addNutrition(data: PrehranaView): Observable<any> {
-    return this._http.post(`http://localhost:5013/Users/PostNutrition`, data);
+    const headers = this.getHeaders();
+    return this._http.post(`http://localhost:5013/Users/PostNutrition`, data, { headers });
   }
 
   addFood(data: HranaView): Observable<any> {
-    return this._http.post(`http://localhost:5013/Users/PostFood`, data);
+    const headers = this.getHeaders();
+    return this._http.post(`http://localhost:5013/Users/PostFood`, data, { headers });
   }
 
   updateUser(id: number, data: UserView): Observable<any> {
-    return this._http.put(`http://localhost:5013/Users/EditUser?userId=${id}`, data);
+    const headers = this.getHeaders();
+    return this._http.put(`http://localhost:5013/Users/EditUser?userId=${id}`, data, { headers });
   }
   
   updateNutrition(id: number, data: PrehranaView): Observable<any> {
-    return this._http.put(`http://localhost:5013/Users/EditNutrition?nutritionId=${id}`, data);
+    const headers = this.getHeaders();
+    return this._http.put(`http://localhost:5013/Users/EditNutrition?nutritionId=${id}`, data, { headers });
   }
 
   deleteUser(id: number): Observable<any> {
-    return this._http.delete(`http://localhost:5013/Users/DeleteUser?userId=${id}`);
+    const headers = this.getHeaders();
+    return this._http.delete(`http://localhost:5013/Users/DeleteUser?userId=${id}`, { headers });
   }
 
   deleteNutrition(id: number): Observable<any> {
-    return this._http.delete(`http://localhost:5013/Users/DeleteNutrition?nutritionId=${id}`);
+    const headers = this.getHeaders();
+    return this._http.delete(`http://localhost:5013/Users/DeleteNutrition?nutritionId=${id}`, { headers });
   }
 
   deleteFood(id: number): Observable<any> {
-    return this._http.delete(`http://localhost:5013/Users/DeleteFood?foodId=${id}`);
+    const headers = this.getHeaders();
+    return this._http.delete(`http://localhost:5013/Users/DeleteFood?foodId=${id}`, { headers });
   }
 
   setUsername(data: UsernameView): Observable<any> {
-    return this._http.post(`http://localhost:5013/Users/EditUsername`, data);
+    const headers = this.getHeaders();
+    return this._http.post(`http://localhost:5013/Users/EditUsername`, data, { headers });
   }
 
   setPassword(data: PasswordView): Observable<any> {
-    return this._http.post(`http://localhost:5013/Users/EditPassword`, data);
+    const headers = this.getHeaders();
+    return this._http.post(`http://localhost:5013/Users/EditPassword`, data, { headers });
   }
 
   setRole(data: RoleView): Observable<any> {
-    return this._http.post(`http://localhost:5013/Users/EditRole`, data);
+    const headers = this.getHeaders();
+    return this._http.post(`http://localhost:5013/Users/EditRole`, data, { headers });
   }
 }
